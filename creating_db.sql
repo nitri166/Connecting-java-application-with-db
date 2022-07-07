27408ef3f815c27bfd2b00d1abdb6984a69a250e
Neetika Tripathi <64384522+nitri166@users.noreply.github.com>
2022-07-08 00:48:59 +0530

    Creating database in My SQL

---
+++ creating_db.sql
@@ -0,0 +1,18 @@
+create database neetika;
+use neetika;
+create table newtable(StudentName varchar(30),
+CollegeName varchar(50),
+Round1Marks float(10),
+Round2Marks float,
+Round3Marks float,
+TechnicalRoundMarks float,
+TotalMarks float,
+Result varchar(20)
+);
+
+
+
+
+
+
+
